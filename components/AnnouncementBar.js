c185e8e8177ee87f510fb1267f9cd714acf6924a
SHAFIN SHAIKH <shafinshaikh25@gmail.com>
2024-07-12 17:50:14 +0530

    active button in annbar

--- components/AnnouncementBar.js
+++ components/AnnouncementBar.js
@@ -1,11 +1,11 @@
-'use client';
+"use client";
 
-import { useState, useEffect } from 'react';
-import { fetchNetworkStats } from '@/lib/api';
+import { useState, useEffect } from "react";
+import { fetchNetworkStats } from "@/lib/api";
 
 export default function AnnouncementBar() {
   const [stats, setStats] = useState(null);
-
+  
   useEffect(() => {
     const fetchStats = async () => {
       const data = await fetchNetworkStats();
@@ -19,11 +19,17 @@ export default function AnnouncementBar() {
   if (!stats) return null;
 
   return (
-    <div className="bg-gray-950 text-white py-2 px-2 flex  justify-evenly text-sm">
+    <div className="bg-gray-950 text-white py-3 px-2 flex  justify-evenly text-sm">
       <span>SOL: ${stats.solPrice}</span>
       <span>mCap: ${stats.marketCap}</span>
-      <span>TPS: {stats.tps}</span>
-      <span>Network: Online</span>
+      <span>Current TPS: {stats.tps}</span>
+      <span>
+        Network:{" "}
+        <span className="inline-flex items-center bg-green-100 text-green-800 text-xs font-medium px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300">
+          <span className="w-2 h-2 me-1 bg-green-500 rounded-full"></span>
+          Active
+        </span>
+      </span>
     </div>
   );
 }
