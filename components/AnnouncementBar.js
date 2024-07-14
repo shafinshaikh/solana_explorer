f62897ec115e5da584011978f910c8293b847412
SHAFIN SHAIKH <shafinshaikh25@gmail.com>
2024-07-14 15:31:25 +0530

    annbar mobres

--- components/AnnouncementBar.js
+++ components/AnnouncementBar.js
@@ -22,10 +22,10 @@ export default function AnnouncementBar() {
 
   return (
     <div className="bg-gray-950 text-white py-3 px-2 flex  justify-evenly text-sm">
-      <span>SOL: ${stats.solPrice}</span>
-      <span>mCap: ${stats.marketCap}</span>
+      <span className="hidden sm:inline">SOL: ${stats.solPrice}</span>
+      <span className="hidden sm:inline">mCap: ${stats.marketCap}</span>
       <span>Current TPS: {stats.tps}</span>
-      <span>
+      <span className="hidden sm:inline">
         Network:{" "}
         <span className="inline-flex items-center bg-green-100 text-green-800 text-xs font-medium px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300">
           <span className="w-2 h-2 me-1 bg-green-500 rounded-full"></span>
